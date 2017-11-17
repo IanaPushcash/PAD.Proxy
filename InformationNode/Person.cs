960c64b16993d8a5ec10c07e8b5c1ac820acd523
IanaPushcash <anny_ok-94@mail.ru>
2017-11-17 12:07:54 +0200

    verifier xml json

--- InformationNode/Person.cs
+++ InformationNode/Person.cs
@@ -3,7 +3,7 @@
 	public class Person
 	{
 		public string Name { get; set; }
-		public int Age { get; set; }
+		public int? Age { get; set; }
 		public string Country { get; set; }
 
 		public Person() { }
--- lab2/Person.cs
+++ lab2/Person.cs
@@ -9,12 +9,12 @@ namespace lab2
 	public class Person
 	{
 		public string Name { get; set; }
-		public int Age { get; set; }
+		public int? Age { get; set; }
 		public string Country { get; set; }
 
 		public Person() { }
 
-		public Person(string name, int age, string country)
+		public Person(string name, int? age, string country)
 		{
 			Name = name;
 			Age = age;
